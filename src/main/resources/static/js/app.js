0185c075f669af8e395a50a1688d51d7ab3bcb56
jobebabou <jobisjb@yahoo.com>
2022-06-27 18:17:34 +0100

    sorting and loading data on page completed

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,22 +1,32 @@
+const element = document.getElementById("sortOptions");
 window.addEventListener("load", loadTable);
+element.addEventListener("click", loadTable);
+
 
 function loadTable() {
-    const url = `/api/v1/players/all`;
+    let select = document.getElementById("sortOptions").value;
+    let url = "";
+    if(select != undefined){
+        url = "api/v1/players/"+select;
+    }else {
+        url = "api/v1/players/all";
+    }
+
     let text = "";
     fetch(url)
         .then(response => response.json())
         .then(function (object) {
-            console.log(object);
-                 text += "<tr>"+
-                     "<td>"+data[dataKey].name+"</td>"
-                 "<td>"+data[dataKey].num+"</td>"
-                 "</tr>";
-
-
+            //console.log(object);
+            for (const objectKey in object) {
+                text += "<tr>"+
+                    "<td>"+object[objectKey].name+"</td>"+
+                    "<td>"+object[objectKey].num+"</td>"+
+                    "</tr>";
+            }
             document.getElementById("renderHere").innerHTML = text;
         })
         .catch((err) => {
-            text += ` `;
+            text += `No data`;
             document.getElementById("renderHere").innerHTML = text;
         })
 }
