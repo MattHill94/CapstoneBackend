43f9c125e6fb33fe0e43d3bfcf95813d08951605
Matt Hill <matthill94.dev@hotmail.com>
2022-07-04 16:44:57 +0100

    Added percentage sort methods and tests

--- src/main/java/com/example/demo/tacos/DataPDFExporter.java
+++ src/main/java/com/example/demo/tacos/DataPDFExporter.java
@@ -1,20 +1,15 @@
 package com.example.demo.tacos;
 
-import com.lowagie.text.DocumentException;
 import com.lowagie.text.Font;
-import com.lowagie.text.FontFactory;
-import com.lowagie.text.PageSize;
-import com.lowagie.text.Phrase;
+import com.lowagie.text.*;
 import com.lowagie.text.pdf.PdfPCell;
 import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfWriter;
 
 import javax.servlet.http.HttpServletResponse;
+import java.awt.*;
 import java.io.IOException;
-import java.awt.Color;
 import java.util.List;
-
-import com.lowagie.text.*;
 
 
 public class DataPDFExporter {
@@ -33,17 +28,22 @@ public class DataPDFExporter {
         font.setColor(Color.WHITE);
 
         cell.setPhrase(new Phrase("Number", font));
-
         table.addCell(cell);
 
         cell.setPhrase(new Phrase("Full Name", font));
         table.addCell(cell);
+
+        cell.setPhrase(new Phrase("Task completion", font));
+        table.addCell(cell);
+
     }
 
     private void writeTableData(PdfPTable table) {
         for (Player player : this.player) {
             table.addCell(String.valueOf(player.getNum()));
             table.addCell(player.getName());
+            table.addCell(player.getPercentage() + "%");
+
         }
     }
 
@@ -56,14 +56,14 @@ public class DataPDFExporter {
         font.setSize(18);
         font.setColor(Color.BLUE);
 
-        Paragraph p = new Paragraph("List of Players", font);
+        Paragraph p = new Paragraph("Troop report", font);
         p.setAlignment(Paragraph.ALIGN_CENTER);
 
         document.add(p);
 
-        PdfPTable table = new PdfPTable(2);
+        PdfPTable table = new PdfPTable(3);
         table.setWidthPercentage(100f);
-        table.setWidths(new float[] {1.5f, 10.0f});
+        table.setWidths(new float[] {1.5f, 8.5f, 1.5f});
         table.setSpacingBefore(10);
 
         writeTableHeader(table);
