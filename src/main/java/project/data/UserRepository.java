34bc69ec2d62ca7496bf0c28d273ec8d6a61d57c
Matt Hill <matthill94.dev@hotmail.com>
2022-07-19 10:33:29 +0100

    Added comments

--- src/main/java/project/data/UserRepository.java
+++ src/main/java/project/data/UserRepository.java
@@ -3,8 +3,10 @@ package project.data;
 import org.springframework.data.repository.CrudRepository;
 import project.User;
 
+
 public interface UserRepository extends CrudRepository<User, Long> {
 
+    // Finds user by username
     User findByUsername(String username);
 
 }
